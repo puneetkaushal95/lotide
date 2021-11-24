94221c5b2891630087b03832013d8e3dff93f331
puneetkaushal95 <puneet.kaushal95@gmail.com>
2021-11-24 01:58:50 +0000

    assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,5 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual,expected)) {
     console.log("✅✅✅ Assertion Passed:", actual, "===" ,expected,);
   } else {
     console.log("🛑🛑🛑 Assertion Failed:", actual, "!==" ,expected);
@@ -10,13 +10,12 @@ const eqArrays = function(actual, expected){
   for (let i = 0; i < expected.length; i++) {
     if(actual[i] !== expected[i]){
       return false;
-    } else{
     }
   }
   return true;
 }
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
