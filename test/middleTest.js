1facfdea474c8a660bd4f7c97752a341185d226c
puneetkaushal95 <puneet.kaushal95@gmail.com>
2021-11-29 19:40:21 +0000

    Refactoring files

---
+++ test/middleTest.js
@@ -0,0 +1,13 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+const eqArrays = require('../eqArrays');
+
+const middle = require('../middle');
+
+
+middle([1]);
+middle([1, 2]);
+middle([1, 2, 3]);
+middle([1, 2, 3, 4, 5]);
+middle([1, 2, 3, 4]);
+middle([1, 2, 3, 4, 5, 6]);
