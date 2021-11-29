b47017820231067060bd47489bb16d15f63d7a96
puneetkaushal95 <puneet.kaushal95@gmail.com>
2021-11-29 19:07:41 +0000

    Refactoring the files

--- eqArrays.js
+++ eqArrays.js
@@ -1,3 +1,5 @@
+const assertEqual = require('./assertEqual');
+
 const eqArrays = function(actual, expected){
   for (let i = 0; i < expected.length; i++) {
     if(actual[i] !== expected[i]){
@@ -8,15 +10,4 @@ const eqArrays = function(actual, expected){
   return true;
 }
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed:", actual, "===" ,expected,);
-  } else {
-    console.log("🛑🛑🛑 Assertion Failed:", actual, "!==" ,expected);
-  }
-};
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = eqArrays;
