ca3d66fc62349dde546b925faa944d368c093db6
puneetkaushal95 <puneet.kaushal95@gmail.com>
2021-11-29 18:59:37 +0000

    Refactoring the files

--- tail.js
+++ tail.js
@@ -1,15 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed:", actual, "===" ,expected,);
-  } else {
-    console.log("🛑🛑🛑 Assertion Failed:", actual, "!==" ,expected);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const tail = function(array) {
   return (array.slice(0));
 };
 
-
-assertEqual(tail([5,6,7]), 6);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = tail;
